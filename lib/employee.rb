438d9e45487c435de04af963593497dfa86a32c8
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-27 09:58:03 -0700

    Employee#validate

--- lib/employee.rb
+++ lib/employee.rb
@@ -15,4 +15,16 @@ class Employee
     @start_date = Date.parse start_date
     @end_date = Date.parse end_date
   end
+
+  def self.validate(data)
+    return false if data.length != 5
+    return false if data[0].to_i.to_s != data[0]
+    begin
+      Date.parse(data[3])
+      Date.parse(data[4])
+    rescue ArgumentError
+      return false
+    end
+    true
+  end
 end
