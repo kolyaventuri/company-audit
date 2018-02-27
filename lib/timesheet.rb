d2c19f82a147a835ae946bdfd71f5f6dda855ad4
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-27 09:31:21 -0700

    Timesheet tests

--- lib/timesheet.rb
+++ lib/timesheet.rb
@@ -1,4 +1,14 @@
 # Defines a timesheet
 class Timesheet
+  attr_reader :employee_id,
+              :project_id,
+              :date,
+              :minutes
 
+  def initialize(employee_id, project_id, date, minutes)
+    @employee_id = employee_id.to_i
+    @project_id = project_id.to_i
+    @date = Date.parse date
+    @minutes = minutes.to_i
+  end
 end
--- test/timesheet_test.rb
+++ test/timesheet_test.rb
@@ -18,13 +18,13 @@ class TimesheetTest < Minitest::Test
 
   def test_attributes
     assert_equal 5, @timesheet.employee_id
-    assert_instance_of Integer, @timesheet.emplyee_id
+    assert_instance_of Integer, @timesheet.employee_id
 
     assert_equal 7, @timesheet.project_id
     assert_instance_of Integer, @timesheet.project_id
 
     assert_equal Date.new(2015, 1, 1), @timesheet.date
-    assert_instance_of Integer @timesheet.minutes
+    assert_instance_of Integer, @timesheet.minutes
     assert_equal 120, @timesheet.minutes
   end
 end
