57602204e136931a766cb6aabfed668bd93bf62c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-27 10:57:03 -0700

    Company loading

--- test/audit_test.rb
+++ test/audit_test.rb
@@ -9,9 +9,19 @@ class AuditTest < Minitest::Test
   def setup
     @audit = Audit.new
     @company = Company.new
+
+    @company.load_employees('./data/employees.csv')
+    @company.load_projects('./data/projects.csv')
+    @company.load_timesheets('./data/timesheets.csv')
   end
 
   def test_it_exists
     assert_instance_of Audit, @audit
+  end
+
+  def test_can_load_company
+    expected = @audit.load_company(@company)
+
+    assert_instance_of Company, expected
   end
 end
