73ddeec984ddd9a6669156214a668dcf91586296
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-27 09:49:38 -0700

    Initial employee loading, no error checking

--- lib/company.rb
+++ lib/company.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'csv'
+
+require_relative 'employee'
+
 # Defines a company
 class Company
   attr_reader :employees,
@@ -10,5 +14,23 @@ class Company
     @employees = []
     @projects = []
     @timesheets = []
+  end
+
+  def load_employees(filename)
+    CSV.foreach(
+      filename
+    ) do |employee_info|
+      employee = Employee.new(
+        employee_info[0],
+        employee_info[1],
+        employee_info[2],
+        employee_info[3],
+        employee_info[4]
+      )
+
+      @employees.push(employee)
+    end
+
+    { success: true, error: nil}
   end
 end
