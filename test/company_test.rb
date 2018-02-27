2137fffdc50d804e36bafb2afcbb6160a7438e8a
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-27 10:08:44 -0700

    Load projects tests

--- test/company_test.rb
+++ test/company_test.rb
@@ -38,4 +38,24 @@ class CompanyTest < Minitest::Test
 
     assert_equal 0, @company.employees.length
   end
+
+  def test_can_load_projects
+    expected = @company.load_projects './data/projects.csv'
+    assert_instance_of Hash, expected
+
+    assert_equal true, expected[:success]
+    assert_nil expected[:error]
+
+    assert_equal 2, @company.projects.length
+  end
+
+  def test_does_reject_bad_employees
+    expected = @company.load_projects './data/bad_projects.csv'
+    assert_instance_of Hash, expected
+
+    assert_equal false, expected[:success]
+    assert_equal 'bad data', expected[:error]
+
+    assert_equal 0, @company.projects.length
+  end
 end
